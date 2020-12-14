108101eb35103fb297721ba2c924fc82a38918a9
emile ferrand <emile@storied.co>
2020-12-13 16:49:33 -0800

    Update cors whitelist

--- server/server.js
+++ server/server.js
@@ -25,7 +25,16 @@ const locationRoute = require("./routes/location.route");
 const app = express();
 
 // enable cors
-app.use(cors({ origin: 'http://localhost:8080', credentials: true }));
+var whitelist = ['http://localhost:8080', 'https://emile-f.github.io/craigslist-explorer/']
+app.use(cors({
+  origin: function (origin, callback) {
+    if (whitelist.indexOf(origin) !== -1) {
+      callback(null, true)
+    } else {
+      callback(new Error('Not allowed by CORS'))
+    }
+  }, credentials: true
+}));
 
 // init cookie-parser
 app.use(cookieParser());
